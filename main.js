9004cfd0333871c20ab45237c26b3d2735550028
WilloBirillo <174868022+WilloBirillo@users.noreply.github.com>
2024-09-27 16:21:09 +0200

    Add: function to select right calculator function

--- main.js
+++ main.js
@@ -14,4 +14,19 @@ function divideParameters(num1, num2) {
     return num1 / num2;
 }
 
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(num1, num2, operator) {
+    if (operator === "+") {
+        addParameters(num1, num2);
+    } else if( operator === "-"){
+        subParameters(num1, num2);
+    } else if (operator === "*"){
+        multiplyParameters(num1, num2);
+    } else if (operator === "/"){
+        divideParameters(num1, num2);
+    }
+}
 
