d5908c135748ce4ac25668a1b87c422f6fb46a21
WilloBirillo <174868022+WilloBirillo@users.noreply.github.com>
2024-09-27 16:14:03 +0200

    Add: four basic calculator function

---
+++ main.js
@@ -0,0 +1,17 @@
+function addParameters(num1, num2) {
+    return num1 + num2;
+}
+
+function subParameters(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplyParameters(num1, num2) {
+    return num1 * num2;
+}
+
+function divideParameters(num1, num2) {
+    return num1 / num2;
+}
+
+
